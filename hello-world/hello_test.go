204041085d74043352431a05c3379dfcc27cbafc
Omkar <omkarak45@gmail.com>
2025-01-21 23:25:37 +0530

    feat: improve tests

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("world")
+		want := "Hello, world"
+		
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("saying hello world when empty string is passed", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, world"
+		
+		assertCorrectMessage(t,got,want)
+	})
+}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	
+	if got != want { 
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
